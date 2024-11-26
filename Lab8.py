c6dbca1e07f5719fd95a75b835299584f04ff9c7
JenniferOji <g00413791@atu.ie>
2024-11-26 09:57:16 +0000

    4 images added to screen

--- Lab8.py
+++ Lab8.py
@@ -11,7 +11,7 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # to control the amount of rows and columns 
 nrows = 2
-ncols = 1
+ncols = 2
 
 # the images on top 
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
@@ -19,7 +19,12 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 
 plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('Gray Scale'), plt.xticks([]), plt.yticks([])
-plt.show()
 
+# the images on top
+plt.subplot(nrows, ncols,3),plt.imshow(gray_image, cmap = 'gray')
+plt.title('3 x 3'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(nrows, ncols,4),plt.imshow(gray_image, cmap = 'gray')
+plt.title('5 x 5'), plt.xticks([]), plt.yticks([])
 
 plt.show() 
