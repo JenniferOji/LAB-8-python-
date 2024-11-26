35e4525d054c37b8291b348f5cf882d7ba57d53d
JenniferOji <g00413791@atu.ie>
2024-11-26 09:50:35 +0000

    Images plotted to screen

---
+++ Lab8.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
+
+img = cv2.imread('ATU.jpg',)
+
+# # Convert the image to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# to control the amount of rows and columns 
+nrows = 2
+ncols = 2
+
+# the images on top 
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+plt.show() 
