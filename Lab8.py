c4802194461d1f81e924af58fd60c31c0d73f7b9
JenniferOji <g00413791@atu.ie>
2024-11-26 10:00:49 +0000

    bottom images blurred

--- Lab8.py
+++ Lab8.py
@@ -13,18 +13,24 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 nrows = 2
 ncols = 2
 
-# the images on top 
+# the images on top normal and grayscale
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
 plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('Gray Scale'), plt.xticks([]), plt.yticks([])
 
-# the images on top
-plt.subplot(nrows, ncols,3),plt.imshow(gray_image, cmap = 'gray')
+#adding the blur 
+imgOut = cv2.GaussianBlur(gray_image,(3, 3),0)
+
+# gray scale images on the bottom 
+plt.subplot(nrows, ncols,3),plt.imshow(imgOut, cmap = 'gray')
 plt.title('3 x 3'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(nrows, ncols,4),plt.imshow(gray_image, cmap = 'gray')
-plt.title('5 x 5'), plt.xticks([]), plt.yticks([])
+# adding a blur 
+imgOut2 = cv2.GaussianBlur(gray_image,(13, 13),0)
+
+plt.subplot(nrows, ncols,4),plt.imshow(imgOut2, cmap = 'gray')
+plt.title('13 x 13'), plt.xticks([]), plt.yticks([])
 
 plt.show() 
